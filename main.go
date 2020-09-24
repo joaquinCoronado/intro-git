242372d33c66d3644cc0d2b24d65feeac907da5f
Joaquin Coronado <joaquincora@hotmail.com>
2020-09-24 08:32:30 -0500

    Conflictos solucionados

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hola mundo")
-<<<<<<< HEAD
 	fmt.Println("Este es un mensaje desde la rama 1")
-=======
 	fmt.Println("Mensaje desde la rama 2")
->>>>>>> rama2
 }
